3c14d5f926d0ae90d925a3d14cb4db82df7eb1c3
Dev-5804 <dev.endra.mali.5804@gmail.com>
2026-07-24 18:07:01 +0530

    Simplify reverseString method by removing unnecessary conditionals

--- 0344_Reverse_String/Solution.cpp
+++ 0344_Reverse_String/Solution.cpp
@@ -12,17 +12,10 @@ public:
 
         for (int i = 0; i < j; i++)
         {
-            if (i < j)
-            {
-                char temp = s[i];
-                s[i] = s[j];
-                s[j] = temp;
-                j--;
-            }
-            else
-            {
-                break;
-            }
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j--;
         }
     }
 };
